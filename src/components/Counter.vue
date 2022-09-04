27be5ad4ff06324061afdaca2dd9f7accfcd547a
fancn21th <fancn21th@gmail.com>
2022-09-04 10:46:21 +0800

    very basic api

---
+++ src/components/Counter.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Count: {{ state.count }}</h1>
+  <h2>Doubled Count: {{ state.doubledCount }}</h2>
+  <button @click="incrementCount">Counter + 1</button>
+</template>
+
+<script setup>
+import { reactive, computed } from "vue";
+
+const state = reactive({
+  count: 0,
+  doubledCount: computed(() => state.count * 2),
+});
+
+const incrementCount = () => {
+  state.count += 1;
+};
+</script>
